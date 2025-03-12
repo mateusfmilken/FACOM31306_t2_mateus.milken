6cab120f235a5275b0d797df964c08a9f3d41636
mateus.milken <169017881+mateusfmilken@users.noreply.github.com>
2025-03-12 20:35:46 -0300

    LAB04 ex01 resolvido

--- LAB04 - Inheritance/ex01/ex01/src/Main.java
+++ LAB04 - Inheritance/ex01/ex01/src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
         CandidatoVereador c1 = new CandidatoVereador("Adenildo José Leodino da Silva", "29/12/1995", "Masculino", 0.00, "Não", 12 , "111", "Joaquim Gomes");
         CandidatoPrefeito c2 = new CandidatoPrefeito("Alexandre Gilberto Sobreira", "18/02/1974", "Masculino", 71545.05, "Não", 15, "Luciana Luna", 15);
-
-        System.out.println("Matrícula: 12321BCC021\nCódigo: 021");
-        System.out.println("Município: Colônia Leopoldina 2702108");
+        System.out.println("Município: Colônia Leopoldina");
         System.out.println(c1.toString());
         System.out.println(c2.toString());
     }
