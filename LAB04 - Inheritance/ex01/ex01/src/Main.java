772ae4bf8a50c31c811b6b11cbeeec4cffe34e3a
mateus.milken <169017881+mateusfmilken@users.noreply.github.com>
2025-03-12 20:41:30 -0300

    LAB04 Resolvido!

--- LAB04 - Inheritance/ex01/ex01/src/Main.java
+++ LAB04 - Inheritance/ex01/ex01/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         CandidatoVereador c1 = new CandidatoVereador("Adenildo José Leodino da Silva", "29/12/1995", "Masculino", 0.00, "Não", 12 , "111", "Joaquim Gomes");
         CandidatoPrefeito c2 = new CandidatoPrefeito("Alexandre Gilberto Sobreira", "18/02/1974", "Masculino", 71545.05, "Não", 15, "Luciana Luna", 15);
         System.out.println("Município: Colônia Leopoldina");
